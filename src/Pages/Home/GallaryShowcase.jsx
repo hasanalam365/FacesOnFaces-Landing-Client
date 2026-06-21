7ce712c3f0a5874846d571022d2ce7120c36b442
hasanalam365 <hasanalam365@gmail.com>
2026-06-21 09:47:37 +0600

    gallary show case 6 pictures

--- src/Pages/Home/GallaryShowcase.jsx
+++ src/Pages/Home/GallaryShowcase.jsx
@@ -7,16 +7,13 @@ const GallaryShowcase = () => {
     topRight1:
       "https://i.ibb.co.com/k7DPcB9/faces1.jpg",
     topRight2:
-      "https://i.ibb.co.com/mM4p59N/faces2.jpg",
+      "https://i.ibb.co.com/nMdz9x6Z/faces5.jpg",
     bottomLeft:
-      "https://i.ibb.co.com/r23Q8mNP/faces6.jpg",
+      "https://i.ibb.co.com/VpLSc3Pf/faces4.jpg",
       
     center:
-      "https://i.ibb.co.com/vCT1qBMp/faces3.jpg",
-    rightBottom1:
-      "https://i.ibb.co.com/jPjnCTZw/faces1.jpg",
-    rightBottom2:
-      "https://i.ibb.co.com/v46d24Wv/faces4.jpg",
+      "https://i.ibb.co.com/mM4p59N/faces2.jpg",
+  
   };
 
   return (
@@ -32,67 +29,57 @@ const GallaryShowcase = () => {
           </h2>
         </div>
 
-        {/* Gallery Grid */}
-        <div className="grid grid-cols-12 gap-4">
-          {/* Large Image */}
-          <div className="col-span-12 row-span-2 md:col-span-7">
-            <img
-              src={images.large}
-              alt=""
-              className="w-full h-[480px] object-cover rounded-xl"
-            />
-          </div>
+       {/* Gallery Grid */}
+<div className="grid grid-cols-12 gap-4">
+  {/* Large Hero Image */}
+  <div className="col-span-12 md:col-span-8">
+    <img
+      src={images.large}
+      alt=""
+      className="w-full h-[500px] object-cover rounded-xl"
+    />
+  </div>
 
-          {/* Top Right Images */}
-          <div className="col-span-6 md:col-span-5">
-            <img
-              src={images.topRight1}
-              alt=""
-              className="w-full h-[250px] md:h-[200px] lg:h-[200px] object-cover rounded-xl"
-            />
-          </div>
+  {/* Right Stack */}
+  <div className="flex flex-col col-span-12 gap-4 md:col-span-4">
+    <img
+      src={images.topRight1}
+      alt=""
+      className="w-full h-[242px] object-cover rounded-xl"
+    />
 
-          <div className="col-span-6 md:col-span-5">
-            <img
-              src={images.topRight2}
-              alt=""
-              className="w-full h-[250px] object-cover rounded-xl"
-            />
-          </div>
+    <img
+      src={images.topRight2}
+      alt=""
+      className="w-full h-[242px] object-cover rounded-xl"
+    />
+  </div>
 
-          {/* Bottom Left */}
-          <div className="col-span-12 md:col-span-3">
-            <img
-              src={images.bottomLeft}
-              alt=""
-              className="w-full h-[376px] object-cover rounded-xl"
-            />
-          </div>
+  {/* Bottom Row */}
+  <div className="col-span-12 md:col-span-3">
+    <img
+      src={images.bottomLeft}
+      alt=""
+      className="w-full h-[260px] object-cover rounded-xl"
+    />
+  </div>
 
-          {/* Bottom Center */}
-          <div className="col-span-12 md:col-span-6">
-            <img
-              src={images.center}
-              alt=""
-            className="w-full h-[376px] object-cover rounded-xl"
-            />
-          </div>
+  <div className="col-span-12 md:col-span-6">
+    <img
+      src={images.center}
+      alt=""
+      className="w-full h-[260px] object-cover rounded-xl"
+    />
+  </div>
 
-          {/* Bottom Right - Two Vertical Images */}
-          <div className="flex flex-row col-span-12 gap-4 md:flex-col lg:flex-col md:col-span-3">
-            <img
-              src={images.rightBottom1}
-              alt=""
-              className="w-full h-[180px] object-cover rounded-xl"
-            />
-
-            <img
-              src={images.rightBottom2}
-              alt=""
-              className="w-full h-[180px] object-cover rounded-xl"
-            />
-          </div>
-        </div>
+  <div className="col-span-12 md:col-span-3">
+    <img
+      src={images.topRight1}
+      alt=""
+      className="w-full h-[260px] object-cover rounded-xl"
+    />
+  </div>
+</div>
       </div>
     </section>
   );
